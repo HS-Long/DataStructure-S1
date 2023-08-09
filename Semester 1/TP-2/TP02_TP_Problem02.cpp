4412ee6ee92201beb2d25029680c3515a68bae5f
HS-Long <141598571+HS-Long@users.noreply.github.com>
2023-08-09 11:18:26 +0700

    Add files via upload

---
+++ Semester 1/TP-2/TP02_TP_Problem02.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+
+    cout << "=====***** TP 02 Data Type, Operators & Math *****=====\n";
+    cout << "Problem-02:============================================\n\n";
+
+    float num, square, cube;
+    cout << "Please input any number: "; cin >> num;
+    square = pow(num,2);
+    cube = pow(num,3);
+    cout << "Square of " << num << " is " << square << " and Cube of " << num << " is " << cube << ".";
+
+}
+    cout << "\n\n";
+    cout << "END Problem-02:=======================================\n\n";
+return 0;
+
